a497728728882374be43aee0972c3f1d0e406bed
coregiu <195412063@qq.com>
2022-10-15 17:15:53 +0800

    fix delay time with loop define in for condition

--- led_loop/led_loop.c
+++ led_loop/led_loop.c
@@ -1,9 +1,9 @@
 #include <mcs51/8051.h>
 
 void delay() {
-    unsigned int i = 512, loop = 10240;
-    for (; i>0; i--){
-        for (;loop>0;loop--) {
+    unsigned int i, j;
+    for (i=0; i < 100; i++){
+        for (j=0; j < 100; j++) {
 
         }
     }
