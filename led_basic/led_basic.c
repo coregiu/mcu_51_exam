3f2b9234e6aa59e8454465b1ad8ef3dbab77cfb9
coregiu <195412063@qq.com>
2022-10-18 23:57:39 +0800

    add ps2 code.

--- led_basic/led_basic.c
+++ led_basic/led_basic.c
@@ -1,19 +1,18 @@
 #include <mcs51/8051.h>
 
-void delay(unsigned int delayTime) {
-    unsigned int loopi;
-    for (delayTime; delayTime > 0; delayTime--){
-        for (loopi = 0; loopi < 100; loopi++){
-
-        }
-    }
+void delayms(unsigned int xms)
+{
+	unsigned int i, j;
+	for (i = xms; i > 0; i--)
+		for (j = 70; j > 0; j--)
+			;
 }
 
 void main(){
     while(1) {
         P0_0 = 0;
-        delay(500);
+        delayms(1000);
         P0_0 = 1;
-        delay(500);
+        delayms(1000);
     }
 }
